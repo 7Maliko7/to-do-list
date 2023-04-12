c6edb4c2c92b68ada34149c433551e3b629e843a
MaLiko <7maliko7maliko7@gmail.com>
2023-04-12 09:22:51 +0300

    DB driver

--- internal/storage/driver/db/db.go
+++ internal/storage/driver/db/db.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"github.com/7Maliko7/to-do-list/internal/storage/structs"
 	"github.com/jackc/pgx/v5"
+	"time"
 )
 
 const (
@@ -34,23 +35,21 @@ func New(ctx context.Context, url string) (DbStorage, error) {
 }
 
 func (fs *DbStorage) CreateTask(req structs.CreateTaskRequest) error {
-	var ok bool
-	err := fs.conn.QueryRow(fs.ctx, "insert into task.list (uuid, name,body,status,deadline) values ($1,$2,$3,$4,$5);", req.Uuid, req.Name, req.Body, req.Status, req.Deadline).Scan(&ok)
+	tag, err := fs.conn.Exec(fs.ctx, "insert into task.list (uuid, name,body,status,deadline) values ($1,$2,$3,$4,$5);", req.Uuid, req.Name, req.Body, req.Status, req.Deadline)
 	if err != nil {
 		return err
 	}
-	if ok {
+	if tag.RowsAffected() > 0 {
 		return nil
 	}
-	return errors.New("Not Inserted")
+	return errors.New("Not Created")
 }
 func (fs *DbStorage) DeleteTask(req structs.DeleteTaskRequest) error {
-	var ok bool
-	err := fs.conn.QueryRow(fs.ctx, "delete from task.list where uuid=$1", req.Uuid).Scan(&ok)
+	tag, err := fs.conn.Exec(fs.ctx, "delete from task.list where uuid=$1", req.Uuid)
 	if err != nil {
 		return err
 	}
-	if ok {
+	if tag.RowsAffected() > 0 {
 		return nil
 	}
 	return errors.New("Not Deleted")
@@ -64,32 +63,47 @@ func (fs *DbStorage) GetTask(req structs.GetTaskRequest) (structs.GetTaskRespons
 	return task, nil
 }
 func (fs *DbStorage) GetListTask(req structs.GetListTaskRequest) (structs.GetListTaskResponse, error) {
-	taskList := structs.GetListTaskResponse{}
-	err := fs.conn.QueryRow(fs.ctx, "select * from task.list").Scan(&taskList.List)
+	var rowCount uint64
+	err := fs.conn.QueryRow(fs.ctx, "select count(Uuid) from task.list").Scan(&rowCount)
 	if err != nil {
 		return structs.GetListTaskResponse{}, err
 	}
+
+	taskList := structs.GetListTaskResponse{}
+	rows, err := fs.conn.Query(fs.ctx, "select * from task.list")
+	if err != nil {
+		return structs.GetListTaskResponse{}, err
+	}
+	taskList.List = make([]structs.Task, 0, rowCount)
+	for rows.Next() {
+		var Uuid string
+		var Name string
+		var Body string
+		var Status bool
+		var Deadline time.Time
+		err := rows.Scan(&Uuid, &Name, &Body, &Status, &Deadline)
+		if err != nil {
+			return structs.GetListTaskResponse{}, err
+		}
+		taskList.List = append(taskList.List, structs.Task{
+			Uuid:     Uuid,
+			Name:     Name,
+			Body:     Body,
+			Status:   Status,
+			Deadline: Deadline,
+		})
+	}
 	return taskList, nil
+
 }
 
 func (fs *DbStorage) UpdateTask(req structs.UpdateTaskRequest) error {
-	var ok bool
-	err := fs.conn.QueryRow(fs.ctx, "update task.list set (name,body,status,deadline) = ($2,$3,$4,$5) where uuid=$1;", req.Uuid, req.Name, req.Body, req.Status, req.Deadline).Scan(&ok)
+	tag, err := fs.conn.Exec(fs.ctx, "update task.list set (name,body,status,deadline) = ($1,$2,$3,$4) where uuid=$5", req.Name, req.Body, req.Status, req.Deadline, req.Uuid)
 	if err != nil {
 		return err
 	}
-	if ok {
+	if tag.RowsAffected() > 0 {
 		return nil
 	}
 	return errors.New("Not Updated")
-}
-
-func (fs *DbStorage) writeFile(data interface{}) error {
-
-	return nil
-}
-
-func (fs *DbStorage) readFile(data interface{}) error {
-
-	return nil
 }
